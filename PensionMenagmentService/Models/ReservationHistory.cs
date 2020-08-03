538a5515fc5354f792f03abf90a7dcc32a6d9dc7
Marcin Wołkowicz <mwolkowicz17@gmail.com>
2020-08-03 16:31:51 +0200

    DateTime_Display_problem_solved

--- PensionMenagmentService/Models/ReservationHistory.cs
+++ PensionMenagmentService/Models/ReservationHistory.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,7 +9,11 @@ namespace PensionMenagmentService.Models
     public class ReservationHistory
     {
         public int ReservationHistoryID { get; set; }
+        [Display(Name = "Checkin Date")]
+        [DataType(DataType.Date)]
         public DateTimeOffset check_in_History { get; set; }
+        [Display(Name = "Checkout Date")]
+        [DataType(DataType.Date)]
         public DateTimeOffset check_out_History { get; set; }
         //public int GuestID_History { get; set; }
         public Guest Guest { get; set; }
