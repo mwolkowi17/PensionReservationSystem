2aab38b244ebe166f9c50957abaa3c872bc63e8a
Marcin Wołkowicz <mwolkowicz17@gmail.com>
2020-08-14 13:35:58 +0200

    Reservation_History_pagination_added

--- PensionMenagmentService/Controllers/ReservationHistoryListController.cs
+++ PensionMenagmentService/Controllers/ReservationHistoryListController.cs
@@ -16,7 +16,7 @@ namespace PensionMenagmentService.Controllers
         {
             _context = context;
         }
-        public IActionResult Index()
+        /*public IActionResult Index()
         {
             var reservationHistoryToDisplay = _context.ReservationHistoryItems
                                              .Include(c => c.Guest)
@@ -27,6 +27,31 @@ namespace PensionMenagmentService.Controllers
                 ReservationHistoryList = reservationHistoryToDisplay
             };
             return View(reservationHistoryVM);
+        }*/
+
+        // now it's get ready for sorting and filtering
+        public async Task<IActionResult> Index(string sortOrder, string currentFilter, string searchString, int? pageNumber)
+        {
+            ViewData["CurrentSort"] = sortOrder;
+            ViewData["NameSortParm"] = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            ViewData["DateSortParm"] = sortOrder == "Date" ? "date_desc" : "Date";
+
+            if (searchString != null)
+            {
+                pageNumber = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+            /* var reservationHistoryToDisplay = from n in _context.ReservationHistoryItems
+                                              select n;*/
+            var reservationHistoryToDisplay = _context.ReservationHistoryItems
+                                              .Include(c => c.Guest)
+                                              .Include(c => c.Room);
+                                            
+            int pageSize = 5;
+            return View(await PaginatedList<ReservationHistory>.CreateAsync(reservationHistoryToDisplay.AsNoTracking(), pageNumber ?? 1, pageSize));
         }
     }
 }
