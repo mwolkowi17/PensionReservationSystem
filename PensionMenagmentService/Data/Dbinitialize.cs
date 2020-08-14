77d1c9b118fc3b2f08abec785de8f81672cc29fc
Marcin Wołkowicz <mwolkowicz17@gmail.com>
2020-08-14 10:27:56 +0200

    Rooms_dbSeed_added_need_more_data

---
+++ PensionMenagmentService/Data/Dbinitialize.cs
@@ -0,0 +1,30 @@
+﻿using PensionMenagmentService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PensionMenagmentService.Data
+{
+    public static class DBbinitialize
+    {
+        public static void Initialize(ApplicationDbContext context)
+        {
+            if (context.Rooms.Any())
+            {
+                return;
+            }
+
+            var rooms = new Room[]
+            {
+                new Room{Number=1, nubmerbeds=(RoomType)1, ReguralPrice=160},
+                new Room{Number=2, nubmerbeds=(RoomType)1, ReguralPrice=160}
+            };
+             foreach (Room n in rooms)
+            {
+                context.Rooms.Add(n);
+            }
+            context.SaveChanges();
+        }
+    }
+}
--- PensionMenagmentService/Program.cs
+++ PensionMenagmentService/Program.cs
@@ -4,8 +4,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using PensionMenagmentService.Data;
 
 namespace PensionMenagmentService
 {
@@ -13,7 +15,25 @@ namespace PensionMenagmentService
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            //CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<ApplicationDbContext>();
+                    DBbinitialize.Initialize(context);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred while seeding the database.");
+                }
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -22,5 +42,6 @@ namespace PensionMenagmentService
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+
     }
 }
